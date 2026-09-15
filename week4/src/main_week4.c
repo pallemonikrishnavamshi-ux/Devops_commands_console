c982619b57f8e897f7ec25ecdc6667f511e3284f
Pallemoni Krishnavamshi <pallemonikrishnavamshi@gmail.com>
2026-09-15 08:50:22 +0000

    Week 4: Added process execution using fork execvp wait

---
+++ week4/src/main_week4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../../include/input.h"
+#include "../../include/parser.h"
+#include "../include/process.h"
+
+int main()
+{
+    char *line;
+    char **tokens;
+
+    printf("=====================================\n");
+    printf("DevOps Commands Console - Week 4\n");
+    printf("=====================================\n");
+
+    while (1)
+    {
+        printf("myshell> ");
+
+        line = read_line();
+
+        if (strcmp(line, "exit") == 0)
+        {
+            free(line);
+            break;
+        }
+
+        tokens = parse_line(line);
+
+        if (tokens[0] != NULL)
+        {
+            execute(tokens);
+        }
+
+        free_tokens(tokens);
+        free(line);
+    }
+
+    printf("Goodbye!\n");
+
+    return 0;
+}
